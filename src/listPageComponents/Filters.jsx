7b6b0255dcf60a68cd9fdf1527f5e0f99e33a73d
atisha-b <ishabhattacharya45@gmail.com>
2024-06-09 02:31:30 +0530

    filters till more half

--- src/listPageComponents/Filters.jsx
+++ src/listPageComponents/Filters.jsx
@@ -10,18 +10,24 @@ import {
 } from "react-icons/fa";
 import { MdAddBox } from "react-icons/md";
 import PropTypes from "prop-types";
+import ReactFlagsSelect from "react-flags-select";
 import Slider from "react-slider";
 
 const Filters = ({ onApplyFilters }) => {
   const [values, setValues] = useState([0, 100]);
   const handleChange = (newValues) => setValues(newValues);
+  const [revenue, setValuesForRevenue] = useState([0, 100]);
+  const handleChangeForRevenue = (newRevenue) => setValuesForRevenue(newRevenue);
+  const [profit, setValuesForProfit] = useState([0, 100]);
+  const handleChangeForProfit = (newProfit) => setValuesForProfit(newProfit);
   const [showAssetTypes, setShowAssetTypes] = useState(false);
   const [selectedAssetTypes, setSelectedAssetTypes] = useState([]);
   const [showPriceRange, setShowPriceRange] = useState(false);
-  const [minPrice, setMinPrice] = useState(0);
-  const [maxPrice, setMaxPrice] = useState(2000000);
+  const [showMoreRange, setShowMoreRange] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState("");
   const dropdownRef = useRef(null);
   const dropdownRefPrice = useRef(null);
+  const dropdownRefMore = useRef(null);
   const [showSortOptions, setShowSortOptions] = useState(false);
 
   const toggleAssetTypes = () => {
@@ -38,29 +44,26 @@ const Filters = ({ onApplyFilters }) => {
 
   const handleClear = () => {
     setSelectedAssetTypes([]);
-    setMinPrice(0);
-    setMaxPrice(2000000);
+
   };
 
   const handleApply = () => {
-    onApplyFilters(selectedAssetTypes, values[0], values[1]);
+    onApplyFilters(selectedAssetTypes, values[0], values[1],revenue[0],revenue[1]);
     setShowAssetTypes(false);
     setShowPriceRange(false);
+    setShowMoreRange(false);
   };
 
   const togglePriceRange = () => {
     console.log(showPriceRange);
     setShowPriceRange(!showPriceRange);
   };
-
-  const handleSliderChange = (event) => {
-    const { name, value } = event.target;
-    if (name === "minPrice") {
-      setMinPrice(Number(value));
-    } else {
-      setMaxPrice(Number(value));
-    }
+  const toggleMoreRange = () => {
+    setShowMoreRange(!showMoreRange);
   };
+  
+
+  
   const toggleSortOptions = () => {
     setShowSortOptions(!showSortOptions);
   };
@@ -76,11 +79,18 @@ const Filters = ({ onApplyFilters }) => {
         setShowAssetTypes(false);
       }
     };
+    const handleClickOutsideForMore = (event) => {
+      if (!dropdownRefMore.current.contains(event.target)) {
+        setShowMoreRange(false);
+      }
+    };
     document.addEventListener("click", handleClickOutsideForPrice);
     document.addEventListener("click", handleClickOutside);
+    document.addEventListener("click", handleClickOutsideForMore);
     return () => {
-      document.removeEventListener("click", handleClickOutsideForPrice);
+      document.removeEventListener("click", handleClickOutsideForMore);
       document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("click", handleClickOutsideForMore);
     };
   }, []);
 
@@ -118,7 +128,7 @@ const Filters = ({ onApplyFilters }) => {
   ];
 
   return (
-    <div className="flex items-center justify-between space-x-4 font-gilroy-light w-full dropdown-container">
+    <div className="flex items-center justify-between space-x-4 font-gilroy-medium w-full dropdown-container">
       <div className="flex items-center space-x-4">
         <div className="relative" ref={dropdownRef}>
           <div
@@ -294,30 +304,181 @@ const Filters = ({ onApplyFilters }) => {
           )}
         </div>
 
-        <div className="relative">
-          <select
-            id="more"
-            name="more"
-            className="font-gilroy-bold appearance-none bg-transparent border-2 border-#D8D8D8 rounded-full px-4 py-2 pr-8 focus:outline-none"
-          >
-            <option value="all">More</option>
-          </select>
-          <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-            <svg
-              className="h-6 w-6 text-gray-500 inline ml-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M6 9l6 6 6-6" />
-            </svg>
+        <div className="relative" ref={dropdownRefMore}>
+  <div
+    className="font-gilroy-bold appearance-none bg-transparent border-2 border-#D8D8D8 rounded-full px-4 py-2 pr-8 focus:outline-none cursor-pointer"
+    onClick={toggleMoreRange}
+  >
+    More
+    <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+      <svg
+        className="h-6 w-6 text-gray-500 inline ml-2"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M6 9l6 6 6-6" />
+      </svg>
+    </div>
+  </div>
+
+  {showMoreRange && (
+    <div className="absolute mt-2 bg-white shadow-lg rounded-lg p-4 w-80 z-10">
+      {/* Revenue Multiple Slider */}
+      <div style={{ padding: "10px" }}>
+        <div className="font-gilroy-medium text-[#190041]">Revenue Multiple</div>
+        <Slider
+          className="slider"
+          value={revenue}
+          onChange={handleChangeForRevenue}
+          min={0}
+          max={100}
+          renderTrack={(props, state) => (
+            <div
+              {...props}
+              className={`slider-track-${state.index + 1} ${props.className}`}
+            />
+          )}
+          renderThumb={(props, state) => (
+            <div
+              {...props}
+              className={`slider-thumb ${
+                state.index === 0 ? "slider-thumb-left" : "slider-thumb-right"
+              }`}
+            />
+          )}
+        />
+        <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+          <div className="text-center">
+            <label htmlFor="minRevenue" className="font-gilroy-medium">Min Revenue:</label>
+            <input
+              type="number"
+              className="border-gray-500 border min-w-[5rem] max-w-[8rem] px-3 py-1 rounded-lg"
+              id="minRevenue"
+              value={revenue[0]}
+              onChange={(e) => handleChangeForRevenue([+e.target.value, revenue[1]])}
+            />
+          </div>
+          <div className="text-center">
+            <label htmlFor="maxRevenue" className="font-gilroy-medium">Max Revenue:</label>
+            <input
+              type="number"
+              className="border-gray-500 border min-w-[5rem] max-w-[8rem] px-3 py-1 rounded-lg"
+              id="maxRevenue"
+              value={revenue[1]}
+              onChange={(e) => handleChangeForRevenue([revenue[0], +e.target.value])}
+            />
           </div>
         </div>
       </div>
+
+      {/* Divider */}
+      <div className="flex justify-between mt-2">
+        <hr style={{ width: '280px', height: '1px', backgroundColor: '#D7D7D7', border: 'none' }} />
+      </div>
+
+      {/* Profit Multiple Slider */}
+      <div style={{ padding: "10px" }}>
+        <div className="font-gilroy-medium text-[#190041]">Profit Multiple</div>
+        <Slider
+          className="slider"
+          value={profit}
+          onChange={handleChangeForProfit}
+          min={0}
+          max={100}
+          renderTrack={(props, state) => (
+            <div
+              {...props}
+              className={`slider-track-${state.index + 1} ${props.className}`}
+            />
+          )}
+          renderThumb={(props, state) => (
+            <div
+              {...props}
+              className={`slider-thumb ${
+                state.index === 0 ? "slider-thumb-left" : "slider-thumb-right"
+              }`}
+            />
+          )}
+        />
+        <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+          <div className="text-center">
+            <label htmlFor="minProfit" className="font-gilroy-medium">Min Profit:</label>
+            <input
+              type="number"
+              className="border-gray-500 border min-w-[5rem] max-w-[8rem] px-3 py-1 rounded-lg"
+              id="minProfit"
+              value={profit[0]}
+              onChange={(e) => handleChangeForProfit([+e.target.value, profit[1]])}
+            />
+          </div>
+          <div className="text-center">
+            <label htmlFor="maxProfit" className="font-gilroy-medium">Max Profit:</label>
+            <input
+              type="number"
+              className="border-gray-500 border min-w-[5rem] max-w-[8rem] px-3 py-1 rounded-lg"
+              id="maxProfit"
+              value={profit[1]}
+              onChange={(e) => handleChangeForProfit([profit[0], +e.target.value])}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-between mt-2">
+        <hr style={{ width: '280px', height: '1px', backgroundColor: '#D7D7D7', border: 'none' }} />
+      </div>
+      <div style={{ padding: "10px" }}>
+        <div className="font-gilroy-medium text-[#190041] mt-3">Location
+
+          <ReactFlagsSelect
+            selected={selectedCountry}
+            onSelect={(code) => setSelectedCountry(code)}
+            searchable
+            searchPlaceholder="Search for a country"
+            className="w-full"
+          />
+        </div>
+      </div>
+      <div className="flex justify-between mt-2">
+        <hr style={{ width: '280px', height: '1px', backgroundColor: '#D7D7D7', border: 'none' }} />
+      </div>
+      <div style={{ padding: "10px" }}>
+        <div className="font-gilroy-medium text-[#190041] mt-3">Annual recurring income
+      </div>
+      </div>
+
+
+
+      {/* Action Buttons */}
+      <div className="flex justify-between mt-4">
+        <button
+          type="button"
+          className="bg-gray-200 text-gray-700 rounded-full px-4 py-2"
+          onClick={() => {
+            handleChangeForRevenue([0, 100]);
+            handleChangeForProfit([0, 100]);
+          }}
+        >
+          Clear
+        </button>
+        <button
+          type="button"
+          className="bg-[#190041] text-white rounded-full px-4 py-2"
+          onClick={handleApply}
+        >
+          Apply
+        </button>
+      </div>
+    </div>
+  )}
+</div>
+</div>
+
+
 
       <div className="flex items-center space-x-8 ml-4">
         <div className="relative">
